347905f977123d36d5ee17f158b15a502d144378
edwardedo03 <edwardahhutauruk@gmail.com>
2026-07-12 18:31:14 +0700

    main feature of login and signup are done

--- script/signup.js
+++ script/signup.js
@@ -1,5 +1,6 @@
 $("#signup-form").on("submit", function (e) {
   e.preventDefault();
+  let invalid = false;
 
   const dataSignup = {
     email: $("#email").val(),
@@ -8,28 +9,48 @@ $("#signup-form").on("submit", function (e) {
     role: $("#role").val(),
   };
 
-  $.ajax({
-    url: "../backend/database/signup_db.php",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(dataSignup),
-    success: function (res) {
-      if (res.success) {
-        alert("Akun berhasil dibuat");
+  if ($("#konfirmasi-password").val().trim() === "") {
+    alert("Mohon Mengisi Verifikasi Kata Sandi");
+    return;
+  } else if (dataSignup.password !== $("#konfirmasi-password").val()) {
+    alert("Konfirmasi Kata Sandi Berbeda");
+    return;
+  }
 
-        $("#signup-form")[0].reset();
+  for (const key in dataSignup) {
+    if (dataSignup[key] === null || dataSignup[key].trim() === "") {
+      invalid = true;
+      break;
+    }
+  }
 
-        setTimeout(() => {
-          window.location.href = "../pages/login.html";
-        }, 1000);
-      } else {
-        alert("gagal: " + res.message);
-      }
-    },
-    error: function (xhr, status, error) {
-      console.log("Status:", xhr.status);
-      console.log("Response:", xhr.responseText);
-      console.log("Error:", error);
-    },
-  });
+  if (invalid) {
+    alert("Tolong isi seluruh form!");
+    return;
+  } else {
+    $.ajax({
+      url: "../backend/database/signup_db.php",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(dataSignup),
+      success: function (res) {
+        if (res.success) {
+          alert("Akun berhasil dibuat");
+
+          $("#signup-form")[0].reset();
+
+          setTimeout(() => {
+            window.location.href = "../pages/login.html";
+          }, 1000);
+        } else {
+          alert("gagal: " + res.message);
+        }
+      },
+      error: function (xhr, status, error) {
+        console.log("Status:", xhr.status);
+        console.log("Response:", xhr.responseText);
+        console.log("Error:", error);
+      },
+    });
+  }
 });
